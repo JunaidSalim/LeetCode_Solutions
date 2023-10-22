e6c3202a99ef63a747d2ae22cb1623a10f12393e
Junaid Saleem <115392538+JunaidSalim@users.noreply.github.com>
2023-10-22 10:30:14 +0500

    Create 7,ReverseInteger.cpp

---
+++ 7,ReverseInteger.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int reverse(int x) {
+        long int num=0;
+        while(x!=0)
+        {
+            if((num*10)< (pow(2,31)-1) && (num*10)> -(pow(2,31))){
+            num =(num *10) +( x %10);
+            x/=10;}
+            else{
+                return 0;
+            }
+        }
+        return num;
+    }
+};
