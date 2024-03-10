eb2587050d29999828a3b23052d4432e3e4270ec
Junaid Saleem <junaidsaleem986@gmail.com>
2024-03-10 21:43:24 +0500

    Update 181.EmployeesEarningMoreThanTheirManager.sql

--- 181.EmployeesEarningMoreThanTheirManager.sql
+++ 181.EmployeesEarningMoreThanTheirManager.sql
@@ -1,2 +1,4 @@
 -- Write your PostgreSQL query statement below
-select name as Employee from Employee e1 where salary>(Select salary from Employee e2 where e1.managerId=e2.id)
+select name as Employee 
+from Employee e1 
+where salary > (Select salary from Employee e2 where e1.managerId=e2.id)
