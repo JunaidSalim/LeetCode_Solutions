e1100c180a0a844acdb4d3e0eb377e9c327a0b84
Junaid Saleem <junaidsaleem986@gmail.com>
2024-07-16 00:13:52 +0500

    Create 3.LongestSubstringWithoutRepeatingCharacters.cpp

---
+++ .cpp/3.LongestSubstringWithoutRepeatingCharacters.cpp
@@ -0,0 +1,20 @@
+class Solution
+{
+public:
+    int lengthOfLongestSubstring(string s)
+    {
+        int n = s.length();
+        int left = 0, right = 0;
+        vector<int> mpp(256, -1);
+        int count = 0;
+        while (right < n)
+        {
+            if (mpp[s[right] != -1])
+                left = max(left, mpp[s[right]] + 1);
+            mpp[s[right]] = right;
+            count = max(count, right - left + 1);
+            right++;
+        }
+        return count;
+    }
+};
