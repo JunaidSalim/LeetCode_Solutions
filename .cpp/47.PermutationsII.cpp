c41b21a5322066ef36833ce5e363f1df805d5041
Junaid Saleem <junaidsaleem986@gmail.com>
2024-07-20 13:01:55 +0500

    Create 47.PermutationsII.cpp

---
+++ .cpp/47.PermutationsII.cpp
@@ -0,0 +1,79 @@
+class Solution
+{
+public:
+    vector<int> nextPermutation(vector<int> &nums)
+    {
+        int n = nums.size();
+        int index = -1;
+
+        for (int i = n - 1; i > 0; --i)
+        {
+            if (nums[i] > nums[i - 1])
+            {
+                index = i - 1;
+                break;
+            }
+        }
+
+        if (index == -1)
+        {
+            reverse(nums.begin(), nums.end());
+            return nums;
+        }
+
+        int smallest_index = index + 1;
+        for (int i = index + 1; i < n; ++i)
+        {
+            if (nums[i] > nums[index] && nums[i] <= nums[smallest_index])
+            {
+                smallest_index = i;
+            }
+        }
+
+        swap(nums[index], nums[smallest_index]);
+
+        reverse(nums.begin() + index + 1, nums.end());
+        return nums;
+    }
+
+    vector<vector<int>> permuteUnique(vector<int> &nums)
+    {
+        vector<vector<int>> result;
+        vector<int> prev = nums;
+        result.push_back(prev);
+        int n = factorial(nums.size());
+        for (int i = 0; i < n - 1; ++i)
+        {
+            vector<int> next_perm = nextPermutation(prev);
+            if (!existsInResult(result, next_perm))
+            {
+                result.push_back(next_perm);
+            }
+            prev = next_perm;
+        }
+        return result;
+    }
+
+private:
+    int factorial(int n)
+    {
+        int result = 1;
+        for (int i = 2; i <= n; ++i)
+        {
+            result *= i;
+        }
+        return result;
+    }
+
+    bool existsInResult(const vector<vector<int>> &result, const vector<int> &perm)
+    {
+        for (const auto &vec : result)
+        {
+            if (vec == perm)
+            {
+                return true;
+            }
+        }
+        return false;
+    }
+};
